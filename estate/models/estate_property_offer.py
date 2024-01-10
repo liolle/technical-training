f6eb425f26ea0fb9349b889009bc58bcba249d4e
liolle <44002141+liolle@users.noreply.github.com>
2024-01-10 07:20:38 +0100

    [Update] estate property - add date_deadline (compute, inverse)

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import api,models,fields
 
 class PropertyOffer(models.Model):
    _name = "estate.property.offer"
@@ -9,6 +9,10 @@ class PropertyOffer(models.Model):
 
    partner_id = fields.Many2one("res.partner", required=True)
    property_id = fields.Many2one("estate.property", required=True)
+
+   validity = fields.Integer(default=7)
+
+   date_deadline = fields.Date(copy=False,compute="_date_deadline",inverse='_inverse_date_deadline')
    
    status = fields.Selection(
         string='Status',
@@ -16,4 +20,19 @@ class PropertyOffer(models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         help=""
     )
+   
+   @api.depends("validity")
+   def _date_deadline(self):
+      for record in self:
+         if record.create_date:
+            record.date_deadline = fields.Date.add(record.create_date,days=record.validity)
+         else:
+            record.date_deadline = fields.Date.add(fields.Date.today(),days=record.validity)
+   
+   def _inverse_date_deadline(self):
+        for record in self:
+         if record.create_date:
+            record.date_deadline = fields.Date.subtract(record.create_date,days=record.validity)
+         else:
+            record.date_deadline = fields.Date.subtract(fields.Date.today(),days=record.validity)
 
